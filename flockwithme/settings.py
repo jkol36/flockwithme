0061e522395c45ce17402be07d8297de00416a3f
jkol36 <jonathankolman@jonathans-air.home>
2014-12-27 14:44:52 -0500

    changed database settings for personal server for testing

--- flockwithme/settings.py
+++ flockwithme/settings.py
@@ -84,9 +84,9 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'Flock',                      # Or path to database file if using sqlite3.
+            'NAME': 'flock_database',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'USER': 'Jon',
+            'USER': 'flock_django',
             'PASSWORD': 'J0nnyb0y123',
             'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
             'PORT': '',                      # Set to empty string for default.
