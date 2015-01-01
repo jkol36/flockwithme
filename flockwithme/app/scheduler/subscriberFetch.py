5827fec3f2d1a3742403cba71f6036ad18032e35
jkol36 <jonathankolman@gmail.com>
2015-01-01 14:25:30 -0500

    subscrberFetcher

--- flockwithme/app/scheduler/subscriberFetch.py
+++ flockwithme/app/scheduler/subscriberFetch.py
@@ -38,7 +38,7 @@ class Worker:
 							tuser.save()
 							tlist_object = TwitterList.objects.get(twitter_id=tlist)
 							tlist_object.is_queried = True
-							new_trelationship = TwitterRelationship.objects.create(action="SUBSCRIBE", twitterList=tlist_object)
+							new_trelationship = TwitterRelationship.objects.create(action="SUBSCRIBE", twitterUser=user, twitterList=tlist_object)
 							tlist.save()
 							#add twitter user as a subscriber
 							tuser.twitterrelationship_set.add(new_trelationship, 'SUBSCRIBE')
@@ -49,7 +49,7 @@ class Worker:
 							tuser = TwitterUser.objects.get(twitter_id=member.id)
 							tlist_object = TwitterList.objects.get(twitter_id=tlist)
 							tlist_object.is_queried = True
-							new_trelationship = TwitterRelationship.objects.create(action="SUBSCRIBE", twitterList=tlist_object)
+							new_trelationship = TwitterRelationship.objects.create(action="SUBSCRIBE", twitterUser=tuser, twitterList=tlist_object)
 							tlist_object.save()
 							tuser.twitterrelationship_set.add(new_trelationship, 'SUBSCRIBE')
 							new_trelationship.save()
