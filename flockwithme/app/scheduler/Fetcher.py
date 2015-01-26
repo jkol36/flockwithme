1c60fd5cf45a98ca3666330e99d8f65ba1516932
jkol36 <jonathankolman@gmail.com>
2015-01-25 21:20:03 -0500

    testing"

--- flockwithme/app/scheduler/Fetcher.py
+++ flockwithme/app/scheduler/Fetcher.py
@@ -98,7 +98,7 @@ class Fetch_Account_Info(Thread):
 				self.socialprofile.save()
 		#3 Clean Followers
 		elif len(self.followers_to_be_added) > 1:
-			self.db_followers = [x.twitterUSer.twitter_id for x in self.socialprofile.get_followers()]
+			self.db_followers = [x.twitterUser.twitter_id for x in self.socialprofile.get_followers()]
 			self.should_add = [x for x in self.followers_to_be_added if x not in self.db_followers]
 			for user in should_add:
 				tuser, _ = TwitterUser.objects.get_or_create(twitter_id = user)
