094914b604dcbc6454887978b3676c570516080c
jkol36 <jonathankolman@gmail.com>
2015-01-26 22:11:26 -0500

    Testing auto-follow functionality

--- flockwithme/app/scheduler/auto_pilot.py
+++ flockwithme/app/scheduler/auto_pilot.py
@@ -228,7 +228,7 @@ class AutoPilot(Thread):
 		self.hashtags = self.profile.hashtags.all()
 		self.already_favorited = [x.twitter_status.twitter_id for x in self.socialprofile.get_favorites()]
 		self.tweets = []
-		for i in hashtags:
+		for i in self.hashtags:
 			self.statusses = [x for x in TwitterStatus.objects.filter(hashtag=i) if x.twitter_id not in self.already_favorited]
 			self.tweets.append(y for y in self.statusses)
 		return self.tweets   
