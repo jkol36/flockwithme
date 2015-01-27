edf7c999d70280634f88aa23769cf575501d1005
jkol36 <jonathankolman@gmail.com>
2015-01-26 21:52:40 -0500

    Testing auto-follow functionality

--- flockwithme/app/scheduler/auto_pilot.py
+++ flockwithme/app/scheduler/auto_pilot.py
@@ -37,79 +37,12 @@ class AutoPilot(Thread):
 		self.api = self.get_api()
 		self.following = self.get_friends()
 		self.followers = self.get_followers()
-		self.following_ids = set(x.twitterUser.twitter_id for x in self.following)
-		self.author_ids = [x.twitter_user.twitter_id for x in self.get_tweets()]
-		self.influencers = self.profile.influencers.all()
-		self.authors_and_followers = []
-		if len(self.influencers) > 0:
-			try:
-				for i in self.influencers:
-					print i
-					self.twitter_ids = self.get_followers_of_influencer(i.id)
-			except Exception, e:
-				self.process_e = self.process_exception(e)
-			if len(self.twitter_ids) > 0:
-				try:
-					for i in self.twitter_ids:
-						self.authors_and_followers.append(i)
-				except Exception, e:
-					self.process_e = self.process_exception(e)
+		print self.followers
+		#self.following_ids = set(x.twitterUser.twitter_id for x in self.following)
+		#self.author_ids = [x.twitter_user.twitter_id for x in self.get_tweets()]
+		#self.influencers = self.profile.influencers.all()
+		#self.authors_and_followers = []
 		
-			else:
-				print 'no influencers'
-		else:
-			print "no influencers"
-		
-		if len(self.author_ids) > 0:
-			try:
-				for i in self.author_ids:
-					self.authors_and_followers.append(i)
-			except Exception, e:
-				self.process_e = self.process_exception(e)
-		else:
-			print "no author_ids"
-
-		self.should_follow = [x for x in self.authors_and_followers if x not in self.following_ids]
-		print self.should_follow
-		#################NOW TIME FOR THE FOLLOWING BITCHES ##############
-		self.followers_count = api.me().followers_count
-		self.friends_count = api.me().friends_count
-		self.followed = []
-		self.follow_limit = len(self.followers_count) - len(self.friends_count)
-		if self.follow_limit >= 1000:
-			self.follow_limit = 800
-		print self.follow_limit
-		self.num_followed = 0
-		#if there's users to follow
-		if len(self.should_follow) > 0: 
-			while self.num_followed < self.follow_limit:
-				for i in self.should_follow:
-					try:
-						self.api.create_friendship(user_id=i)
-					except Exception, e:
-						process_e = self.process_exception(e)
-					self.followed.append(i)
-					self.sleep_action()
-			#if users were followed
-			if len(self.followed) > 0:
-				for i in self.followed:
-					try:
-						self.tuser, _ = TwitterUser.objects.get_or_create(twitter_id=i)
-					except Exception, e:
-						self.process_e = self.process_exception(e)
-					self.tuser.save()
-					self.socialprofile.add_friend(self.tuser)
-			else:
-				self.socialprofile.job_status = "No_Users_To_Follow"
-				self.socialprofile.save()
-		#if there are no users to follow
-		else:
-			self.socialprofile.job_status = "No_Users_To_Follow"
-			self.socialprofile.save()
-		self.socialprofile.job_status = 'Just_Followed'
-		self.socialprofile.friend_count = api.me().friends_count
-		self.socialprofile.follower_count = api.me().followers_count
-		self.socialprofile.save()
 
 
 
