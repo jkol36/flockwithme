9852c3b7c5452267c9e545def25485b2c45dcf63
jkol36 <jonathankolman@gmail.com>
2015-02-04 21:15:49 -0500

    added template add_job.ade

--- flockwithme/core/dashboard/views.py
+++ flockwithme/core/dashboard/views.py
@@ -7,19 +7,25 @@ def index(request):
 	new_followers, days, potential_customers = 0, 0, 0
 	now = timezone.now()
 	favorited_tweets = []
+	friends_ = []
 	for acc in request.user.accounts.all():
 		new_followers += acc.get_followers(socialProfile=acc).count()
 		potential_customers += (acc.get_friends().count() + acc.get_favorites().count())
 		days += ( now - acc.profile.date_joined).days
 		favorites = acc.get_favorites()[:30]
+		friends = acc.get_friends()[:30]
 		for i in favorites:
 			favorited_tweets.append(i.twitterStatus.text.encode('utf-8'))
+		for i in friends:
+			friends_.append(i.twitterUser.screen_name)
+
 	money_saved = 100 * days
 	return render(request, 'dashboard.jade', {
 		'new_followers': new_followers, 
 		'potential_customers': potential_customers,
 		'money_saved': money_saved,
 		'favorites':favorited_tweets,
+		'friends':friends_,
 		})
 @login_required
 
