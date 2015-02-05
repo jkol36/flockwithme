33eaf164c19b619640734b1ad89ac1eb0950670a
jkol36 <jonathankolman@gmail.com>
2015-02-05 17:17:06 -0500

    Table should now be bordered

--- flockwithme/core/dashboard/views.py
+++ flockwithme/core/dashboard/views.py
@@ -13,8 +13,8 @@ def index(request):
 		potential_customers += (acc.get_friends().count() + acc.get_favorites().count())
 		days += ( now - acc.profile.date_joined).days
 		tweets_favorited += (acc.get_favorites().count())
-		favorites = acc.get_favorites().filter('created_at').latest()[:30]
-		friends = acc.get_friends().order_by('created_at')[:30]
+		favorites = acc.get_favorites().filter().order_by('created_at')[:30]
+		friends = acc.get_friends().filter().order_by('created_at')[:30]
 		for i in favorites:
 			favorited_tweets.append(i)
 		for i in friends:
