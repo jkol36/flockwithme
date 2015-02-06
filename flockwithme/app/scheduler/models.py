d3ca162b48f0dd366e6d8aa295857fece50cc069
jkol36 <jonathankolman@gmail.com>
2015-02-06 15:06:26 -0500

    Added meta class to TwitterRelationship to help with ordering TwitterRelationship objects

--- flockwithme/app/scheduler/models.py
+++ flockwithme/app/scheduler/models.py
@@ -128,7 +128,7 @@ class TwitterRelationship(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 
 	class Meta:
-		order_with_respect_to = 'created_at'
+		ordering = ['created_at']
 class Location(models.Model):
 	name = models.CharField(max_length=50, blank=True, null=True)
 	latitude = models.FloatField(blank=True, null=True)
