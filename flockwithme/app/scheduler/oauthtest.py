cd075034ae885d2a582f797d5a15f7ad57976e3d
jkol36 <jonathankolman@gmail.com>
2015-02-05 20:43:03 -0500

    added api status view and url and template

--- flockwithme/app/scheduler/oauthtest.py
+++ flockwithme/app/scheduler/oauthtest.py
@@ -8,7 +8,9 @@ import sys
 class TestApi:
 	def __init__(self, *args, **kwargs): 
 		a, b, c, d = "3Gsg8IIX95Wxq28pDEkA", "LjEPM4kQAC0XE81bgktdHAaND3am9tTllXghn0B639o", "131687768-oQ77P6uNEUfQdRVZvOIg6pzZJb8CcZMyZzbnAqQ9", "K0HWSYtpvrgq5buCwYT6jm9dI2qf5XaX97PRyn9WyvTgs"
-		auth = tweepy.OAuthHandler(a, b)
-		auth.set_access_token(c, d)
-		api = tweepy.API(auth)
-		return dir(api)
+		self.auth = tweepy.OAuthHandler(a, b)
+		self.auth.set_access_token(c, d)
+		self.api = tweepy.API(auth)
+		return super(TestApi, self).__init__(*args, **kwargs)
+	print dir(self.api)
+
