e0071f3290d45fff8515393ef02480b3e7ff8995
jkol36 <jonathankolman@gmail.com>
2015-02-06 15:15:09 -0500

    Added meta class to TwitterRelationship to help with ordering TwitterRelationship objects

--- flockwithme/core/dashboard/views.py
+++ flockwithme/core/dashboard/views.py
@@ -14,7 +14,7 @@ def index(request):
 		days += ( now - acc.profile.date_joined).days
 		tweets_favorited += (acc.get_favorites().count())
 		favorites = acc.get_favorites()[:30]
-		friends = acc.get_friends().filter().order_by('created_at')[:30]
+		friends = acc.get_friends()[:30]
 		for i in favorites:
 			favorited_tweets.append(i)
 		for i in friends:
