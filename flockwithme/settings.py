b68c29179747d523542f5cf0df0a8936642d9662
jkol36 <jonathankolman@gmail.com>
2015-03-09 22:32:17 -0400

    postgres

--- flockwithme/settings.py
+++ flockwithme/settings.py
@@ -90,7 +90,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'flock_db',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'USER': 'jon',
+            'USER': 'Jon',
             'PASSWORD': 'J0nnyb0y123',
             'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
             'PORT': '',                      # Set to empty string for default.
