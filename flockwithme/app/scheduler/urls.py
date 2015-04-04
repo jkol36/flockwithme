946ff93babdfa998c8030470ad9d9055c7185c10
jkol36 <jonathankolman@gmail.com>
2015-04-04 13:14:16 -0400

    startform

--- flockwithme/app/scheduler/urls.py
+++ flockwithme/app/scheduler/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 viewpatterns = patterns('flockwithme.app.scheduler.views',
+	url(r'^auto_dm/$', 'auto_dm', name='auto_dm'),
 	url(r'^show_queue/$', 'show_queue', name='show_queue'),
 	url(r'^delete_job/(?P<pk>\d+)/$', 'delete_job', name='delete_job'),
 	url(r'^analytics/$', 'analytics_view', name='analytics'),
